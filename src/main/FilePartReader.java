15fa03149ac1d310fbffdc798758a633ab02f7f9
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2020-02-03 22:33:37 +0100

    Add template files for this task

---
+++ src/main/FilePartReader.java
@@ -0,0 +1,28 @@
+public class FilePartReader {
+	private String filePath;
+	int fromLine, toLine;
+
+	public FilePartReader(){
+		filePath = "";
+	}
+	public FilePartReader(String filePath, int fromLine, int toLine){
+		setup(filePath, fromLine, toLine);
+	}
+
+	public void setup(String filePath, int fromLine, int toLine){
+		if(fromLine < 1 || fromLine > toLine)
+			throw new IllegalArgumentException("FilePartReader.setup: wrong arguments fromLine and/or toLine!");
+
+		this.filePath = filePath;
+		this.fromLine = fromLine;
+		this.toLine = toLine;
+	}
+
+	public String read(){
+		return "";
+	}
+
+	public String readLines(){
+		return "";
+	}
+}
---
+++ src/main/FileWordAnalyzer.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileWordAnalyzer {
+	FilePartReader reader;
+
+	public List<String> getWordsOrderedAlphabetically(){
+		List<String> words = new ArrayList<>();
+
+		return words;
+	}
+
+	public List<String> getWordsContainingSubstring(String subString){
+		List<String> words = new ArrayList<>();
+
+		return words;
+	}
+
+	public List<String> getStringsWhichPalindromes(){
+		List<String> words = new ArrayList<>();
+
+		return words;
+	}
+}
