2826ca18d7d8093625325b2eb9c88c021c30a3ac
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2020-02-05 17:25:36 +0100

    Add private methods to work for word uniqueness and ignore case

--- src/main/FileWordAnalyzer.java
+++ src/main/FileWordAnalyzer.java
@@ -3,6 +3,10 @@ import java.util.List;
 
 public class FileWordAnalyzer {
 	private FilePartReader reader;
+	/* sets if e.g. 'A' is the same what 'a' or not. Default not: */
+	private boolean caseSensitive = true;
+	/* sets if analyzed words can be repeated or not. Default not: */
+	private boolean readUnique = true;
 
 	public FileWordAnalyzer(){
 		reader = new FilePartReader();
@@ -11,12 +15,33 @@ public class FileWordAnalyzer {
 		reader = new FilePartReader(filePath, fromLine, toLine);
 	}
 
+	private boolean caseInsensitiveContains(List<String> list, String str){
+		return list.stream().anyMatch(str::equalsIgnoreCase);
+	}
+
+	private void insertCaseSensitiveAndUnique(List<String> list, String[] words){
+		for(String word : words){
+			if(!list.contains(word))
+				list.add(word);
+		}
+	}
+
+	private void insertCaseInsensitiveAndUnique(List<String> list, String[] words){
+		for(String word : words){
+			if(!caseInsensitiveContains(list, word) )
+				list.add(word);
+		}
+	}
 	/**
 	 * Returns the words ordered by alphabetically as an ArrayList.
 	 * @return
 	 */
 	public List<String> getWordsOrderedAlphabetically(){
 		List<String> words = new ArrayList<>();
+		String fileContent = reader.readLines();
+
+		if(fileContent.length() < 1)
+			return words;
 
 		return words;
 	}
