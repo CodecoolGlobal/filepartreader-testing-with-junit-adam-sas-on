915a4074788c4c7115d356e6b28d0ebab46f2cf1
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2020-02-05 10:45:23 +0100

    Some examples of FilePartReader - readLines()

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,16 @@
+
 public class Main {
 	public static void main(String[] args){
+		FilePartReader fpr = new FilePartReader("Star_Wars_-_Attack_of_the_Clones.txt", 313, 321);
+		String fileContent = fpr.readLines();
+		System.out.println("\tRows from 313 to 321:");
+		System.out.println(fileContent);
 
+		int fromLine = 751, toLine = 775;
+		fpr.setReadRange(fromLine, toLine);
+		fileContent = fpr.readLines();
+		System.out.println("\tRows from " + fromLine +  " to " + toLine + ":");
+		System.out.println(fileContent);
 	}
 }
+
